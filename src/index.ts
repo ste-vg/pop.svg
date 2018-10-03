1a0a4a5d54ae98d044198ecc9d30120040c88bcf
Steve Gardner <github@ste.vg>
2018-10-03 15:22:50 -0700

    im not sure what I'm doing

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { CommandPath } from "./paths/commands";
 export { PathGuide } from "./paths/pathGuide";
 export { ReferencePath } from "./paths/referencePath";
 export { TranslationPath } from "./paths/translationPath";
+export { Point } from "./paths/Point";
